5f3fa3694e452cac5bc3f671259d89637f4ddf53
Dan Erbland <danerbland@gmail.com>
2019-10-22 12:51:45 -0400

    finished regular stuff

--- bubblesort.js
+++ bubblesort.js
@@ -1,7 +1,24 @@
-let globalCounter;
+function bubbleSort(array) {
+  let maxLength = array.length;
+  while(maxLength > 1){
+    for(let i = 0; i < maxLength - 1; i++){
+      console.log('comparing i:', i, ' array[i]: ', array[i], ' and array[i+1]: ', array[i+1])
+      if(compare(array[i], array[i+1])){
+        console.log('swapping')
+        swap(i, i+1, array);
+      }
+    }
+    maxLength -= 1;
+  }
+  return array;
+}
 
-function bubbleSort(array) {}
+function swap(index1, index2, array) {
+  let num1 = array[index1];
+  array[index1] = array[index2];
+  array[index2] = num1;
+}
 
-function swap() {}
-
-function compare() {}
+function compare(num1, num2) {
+  return num1 > num2;
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,5 @@
 describe("Bubble Sort", function() {
-  beforeAll(function() {
+  beforeEach(function() {
     spyOn(window, "swap").and.callThrough();
     spyOn(window, "compare").and.callThrough();
   });
@@ -17,20 +17,22 @@ describe("Bubble Sort", function() {
     expect(window.compare.calls.count()).toEqual(0);
   });
 
-  it("handles multiple items", function() {
+  it("handles two item arrays", function() {
     let unsortedArray = [2, 1];
     expect(bubbleSort(unsortedArray)).toEqual([1, 2]);
     expect(window.swap.calls.count()).toEqual(1);
-    expect(window.compare.calls.count()).toEqual(2);
+    expect(window.compare.calls.count()).toEqual(1);
   });
 
   it("handles duplicates", function() {
     let unsortedArray = [3, 3, 2, 2, 1, 4, 0];
     expect(bubbleSort(unsortedArray)).toEqual([0, 1, 2, 2, 3, 3, 4]);
   });
+  it("handles multiple items", function() {
+    let unsortedArray = [3, 2, 1, 4];
+    expect(bubbleSort(unsortedArray)).toEqual([1, 2, 3, 4]);
+    expect(window.swap.calls.count()).toEqual(3);
+    expect(window.compare.calls.count()).toEqual(6);
+  });
 });
 
-// it("handles multiple items", function() {
-//   let unsortedArray = [3, 2, 1, 4];
-//   expect(bubbleSort(unsortedArray)).toEqual([1, 2, 3, 4]);
-// });
