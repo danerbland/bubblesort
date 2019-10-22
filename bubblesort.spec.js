caf5a8d785fcabeb3bf0030414bb1edef1fe15ba
Alisa Chan <alisachan.ny@gmail.com>
2019-10-22 12:10:18 -0400

    added spy specs

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -3,18 +3,25 @@ describe("Bubble Sort", function() {
     spyOn(window, "swap").and.callThrough();
     spyOn(window, "compare").and.callThrough();
   });
+
   it("handles an empty array", function() {
     expect(bubbleSort([])).toEqual([]);
+    expect(window.swap.calls.count()).toEqual(0);
+    expect(window.compare.calls.count()).toEqual(0);
   });
 
   it("handles single item array", function() {
     let array = [4];
     expect(bubbleSort(array)).toEqual(array);
+    expect(window.swap.calls.count()).toEqual(0);
+    expect(window.compare.calls.count()).toEqual(0);
   });
 
   it("handles multiple items", function() {
-    let unsortedArray = [3, 2, 1, 4];
-    expect(bubbleSort(unsortedArray)).toEqual([1, 2, 3, 4]);
+    let unsortedArray = [2, 1];
+    expect(bubbleSort(unsortedArray)).toEqual([1, 2]);
+    expect(window.swap.calls.count()).toEqual(1);
+    expect(window.compare.calls.count()).toEqual(2);
   });
 
   it("handles duplicates", function() {
@@ -22,3 +29,8 @@ describe("Bubble Sort", function() {
     expect(bubbleSort(unsortedArray)).toEqual([0, 1, 2, 2, 3, 3, 4]);
   });
 });
+
+// it("handles multiple items", function() {
+//   let unsortedArray = [3, 2, 1, 4];
+//   expect(bubbleSort(unsortedArray)).toEqual([1, 2, 3, 4]);
+// });
